44b8ead7fd2ca6a45b06416edcedbd28b686a6f1
Sergio MeNu <silurio@live.com>
2019-03-31 20:24:18 +0200

    Actualización 2.0
    - Se agrego el panel principal del administrador, incluyendo el administrador de películas y de usuarios.
    - Se dotó de funcionalidad, por medio de JSON, a las páginas de administración de usuarios y películas.
    - Un usuario se puede loguear y pasar a la búsqueda siempre que haya sido dado de alta en el administrador de usuarios.
    - La página de búsqueda muestra la sesión del usuario en turno
    - El login sólo permite acceder al administrador si este tiene el usuario y contraseña, pues se trata de la única persona dada de alta desde el principio
    - La página del video de Interstellar recupera la información de la base de administración de películas (sólo nombre de la película, director y año)

--- search_script.js
+++ search_script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
 	var myIndex = 0;
+
+	var person = sessionStorage.getItem('person');
+	if(person !== null){
+		document.getElementById("person").innerHTML = "Bienvenido, " + person;
+	}
 	
 	function carousel(slides) {
 		var x = document.getElementsByClassName(slides);
@@ -22,109 +27,4 @@ $(document).ready(function(){
 	carousel('mySlides5');
 	carousel('mySlides6');
 	
-/*
-	var min=1; 
-    var max=3; 
-	var random = String(Math.floor(Math.random() * (+max - +min)) + +min);
-
-
-	function carousel_0() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides0");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_0, 3000);    
-	}
-
-	function carousel_1() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides1");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_1, 4000);    
-	}
-
-	function carousel_2() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides2");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_2, 5000);    
-	}
-
-	function carousel_3() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides3");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_3, 3000);    
-	}
-
-	function carousel_4() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides4");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_4, 5000);    
-	}
-
-	function carousel_5() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides5");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_5, 4000);    
-	}
-
-	function carousel_6() {
-		//var i;
-		var x = document.getElementsByClassName("mySlides6");
-		for (var i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		myIndex++;
-		console.log(x.length);
-		if (myIndex > x.length) {myIndex = 1}    
-			x[myIndex-1].style.display = "block";  
-		setTimeout(carousel_6, 3000);    
-	}
-
-	carousel_0();
-	carousel_1();
-	carousel_2();
-	carousel_3();
-	carousel_4();
-	carousel_5();
-	carousel_6();
-*/
 });
