64817a61cf0e2a47d1fc12080f4019179ee126f4
Sergio MeNu <silurio@live.com>
2019-05-12 12:28:47 +0200

    Registro de la base y cambios aplicados por django

--- backend/sitio/sitio/urls.py
+++ backend/sitio/sitio/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""sitio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from streamflix import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path( 'streamflix/' , include( 'streamflix.urls' )),
 ]
